c7166286e08b07b912338bf3867e9bf4474d30bd
Jegr Mahmood <68039452+jegr-97@users.noreply.github.com>
2023-09-07 17:17:32 +0300

    fierbase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:athome/Landing/splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() => runApp(const AtHomeApp());
+Future<void> main() async {
+   WidgetsFlutterBinding.ensureInitialized();
+ 
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const AtHomeApp());
+}
 
 class AtHomeApp extends StatelessWidget {
   const AtHomeApp({super.key});
