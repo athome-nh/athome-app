4d79596570bb45c5e12a6f3bc473aede499d07f2
Jegr Mahmood <68039452+jegr-97@users.noreply.github.com>
2024-05-07 16:18:14 +0300

    fix bug

--- lib/Config/my_widget.dart
+++ lib/Config/my_widget.dart
@@ -1341,7 +1341,7 @@ Widget waitingWiget(BuildContext context) {
 Widget waitingWiget2(BuildContext context) {
   return SizedBox(
     height: getWidth(context, 40),
-    child: Image.asset("LogoLoading.gif"),
+    child: Image.asset("assets/images/LogoLoading.gif"),
     // LoadingIndicator(
     //   indicatorType: Indicator.ballRotateChase,
     //   colors: [
